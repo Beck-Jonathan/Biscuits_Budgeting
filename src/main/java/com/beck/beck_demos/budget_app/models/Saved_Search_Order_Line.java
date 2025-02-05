62182565c00e730fea0ac9cf3544d777a8e4932a
Beck-Jonathan <jjbeck7@gmail.com>
2025-02-05 12:25:10 -0600

    Saved search order line item object and tests

---
+++ src/main/java/com/beck/beck_demos/budget_app/models/Saved_Search_Order_Line.java
@@ -0,0 +1,135 @@
+package com.beck.beck_demos.budget_app.models;
+/**
+ * @ author Jonathan Beck
+ * @ version 1.0
+ * @ since 1.0
+ */
+import org.jetbrains.annotations.NotNull;
+
+public class Saved_Search_Order_Line implements Comparable<Saved_Search_Order_Line> {
+  private Integer Saved_Search_Order_ID;
+  private Integer Line_No;
+  private String Category_ID;
+  private Integer User_ID;
+  private String Search_Phrase;
+  private boolean Is_Active;
+
+  public Saved_Search_Order_Line(){}
+
+  public Saved_Search_Order_Line(Integer Saved_Search_Order_ID, Integer Line_No, String Category_ID, Integer User_ID, String Search_Phrase, boolean Is_Active) {
+
+    this.Saved_Search_Order_ID = Saved_Search_Order_ID;
+    this.Line_No = Line_No;
+    this.Category_ID = Category_ID;
+    this.User_ID = User_ID;
+    this.Search_Phrase = Search_Phrase;
+    this.Is_Active = Is_Active;
+  }
+
+  public Saved_Search_Order_Line(Integer Saved_Search_Order_ID, Integer Line_No) {
+
+    this.Saved_Search_Order_ID = Saved_Search_Order_ID;
+    this.Line_No = Line_No;
+  }
+  public Integer getSaved_Search_Order_ID() {
+    return Saved_Search_Order_ID;
+  }
+  public void setSaved_Search_Order_ID(Integer Saved_Search_Order_ID) {
+    if (Saved_Search_Order_ID<0||Saved_Search_Order_ID>10000){
+      throw new IllegalArgumentException("Saved_Search_Order_ID Can Not Be Negative");
+    }
+    this.Saved_Search_Order_ID = Saved_Search_Order_ID;
+  }
+  public Integer getLine_No() {
+    return Line_No;
+  }
+  public void setLine_No(Integer Line_No) {
+    if (Line_No<0||Line_No>10000){
+      throw new IllegalArgumentException("Line_No Can Not Be Negative");
+    }
+    this.Line_No = Line_No;
+  }
+  public String getCategory_ID() {
+    return Category_ID;
+  }
+  public void setCategory_ID(String Category_ID) {
+    Category_ID = Category_ID.replaceAll("[^A-Za-z0-9 - ]","");
+    if(Category_ID.length()<4){
+      throw new IllegalArgumentException("Category_ID is too short.");
+    }
+    if(Category_ID.length()>100){
+      throw new IllegalArgumentException("Category_ID is too long.");
+    }
+    this.Category_ID = Category_ID;
+  }
+  public Integer getUser_ID() {
+    return User_ID;
+  }
+  public void setUser_ID(Integer User_ID) {
+    if (User_ID<0||User_ID>10000){
+      throw new IllegalArgumentException("User_ID Can Not Be Negative");
+    }
+    this.User_ID = User_ID;
+  }
+  public String getSearch_Phrase() {
+    return Search_Phrase;
+  }
+  public void setSearch_Phrase(String Search_Phrase) {
+    Search_Phrase = Search_Phrase.replaceAll("[^A-Za-z0-9 - ]","");
+    if(Search_Phrase.length()<4){
+      throw new IllegalArgumentException("Search_Phrase is too short.");
+    }
+    if(Search_Phrase.length()>100){
+      throw new IllegalArgumentException("Search_Phrase is too long.");
+    }
+    this.Search_Phrase = Search_Phrase;
+  }
+  public boolean getIs_Active() {
+    return Is_Active;
+  }
+  public void setIs_Active(boolean Is_Active) {
+    this.Is_Active = Is_Active;
+  }
+  @Override
+  public int compareTo(@NotNull Saved_Search_Order_Line o) {
+    if (this.Saved_Search_Order_ID.compareTo(o.Saved_Search_Order_ID)<0){
+      return -1;
+    }
+    else if(this.Saved_Search_Order_ID.compareTo(o.Saved_Search_Order_ID) > 0){
+      return 1;
+    }
+    if (this.Line_No.compareTo(o.Line_No)<0){
+      return -1;
+    }
+    else if(this.Line_No.compareTo(o.Line_No) > 0){
+      return 1;
+    }
+    if (this.Category_ID.compareTo(o.Category_ID)<0){
+      return -1;
+    }
+    else if(this.Category_ID.compareTo(o.Category_ID) > 0){
+      return 1;
+    }
+    if (this.User_ID.compareTo(o.User_ID)<0){
+      return -1;
+    }
+    else if(this.User_ID.compareTo(o.User_ID) > 0){
+      return 1;
+    }
+    if (this.Search_Phrase.compareTo(o.Search_Phrase)<0){
+      return -1;
+    }
+    else if(this.Search_Phrase.compareTo(o.Search_Phrase) > 0){
+      return 1;
+    }
+    if (!this.Is_Active&&o.Is_Active){
+      return -1;
+    }
+    if (this.Is_Active&&!o.Is_Active){
+      return 1;
+    }
+    return 0;
+  }
+
+}
+
---
+++ src/test/java/com/beck/beck_demos/budget_app/models/Saved_Search_Order_LineTest.java
@@ -0,0 +1,203 @@
+package com.beck.beck_demos.budget_app.models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Saved_Search_Order_LineTest {
+
+
+  private Saved_Search_Order_Line _saved_search_order_line;
+  @BeforeEach
+  public void setup(){
+    _saved_search_order_line = new Saved_Search_Order_Line();
+  }
+  @AfterEach
+  public void teardown(){
+    _saved_search_order_line = null;
+  }
+  @Test
+  public void testSaved_Search_Order_LineDefaultConstructorSetsNoVariables(){
+    Saved_Search_Order_Line _saved_search_order_line= new Saved_Search_Order_Line();
+    Assertions.assertNull(_saved_search_order_line.getSaved_Search_Order_ID());
+    Assertions.assertNull(_saved_search_order_line.getLine_No());
+    Assertions.assertNull(_saved_search_order_line.getCategory_ID());
+    Assertions.assertNull(_saved_search_order_line.getUser_ID());
+    Assertions.assertNull(_saved_search_order_line.getSearch_Phrase());
+    Assertions.assertFalse(_saved_search_order_line.getIs_Active());
+  }
+  @Test
+  public void testSaved_Search_Order_LineParameterizedConstructorSetsAllVariables(){
+    Saved_Search_Order_Line _saved_search_order_line= new Saved_Search_Order_Line(
+        6151,
+        3215,
+        "REIgRuBpxqGJOcvVhBWCikBdLECBANExCGNqlJbKQYsohcibNePgcZwtWesYpGvWQapPurnUkQqMucUZmVwWUbIgwUhlRwYWBs",
+        3094,
+        "jiWsOhfihBxCkxyvygcaaTGcQysmaICfaFswOsJmXyYCLiJIClfXgotvDpAEWpFLeRgqvosYtUDPYskZnIUvBGwXbwevADqKop",
+        true
+    );
+    Assertions.assertEquals(6151,_saved_search_order_line.getSaved_Search_Order_ID());
+    Assertions.assertEquals(3215,_saved_search_order_line.getLine_No());
+    Assertions.assertEquals("REIgRuBpxqGJOcvVhBWCikBdLECBANExCGNqlJbKQYsohcibNePgcZwtWesYpGvWQapPurnUkQqMucUZmVwWUbIgwUhlRwYWBs",_saved_search_order_line.getCategory_ID());
+    Assertions.assertEquals(3094,_saved_search_order_line.getUser_ID());
+    Assertions.assertEquals("jiWsOhfihBxCkxyvygcaaTGcQysmaICfaFswOsJmXyYCLiJIClfXgotvDpAEWpFLeRgqvosYtUDPYskZnIUvBGwXbwevADqKop",_saved_search_order_line.getSearch_Phrase());
+    Assertions.assertTrue(_saved_search_order_line.getIs_Active());
+  }
+  @Test
+  public void testSaved_Search_Order_LineKeyedParameterizedConstructorSetsKeyedVariables(){
+    Saved_Search_Order_Line _saved_search_order_line= new Saved_Search_Order_Line(
+        5343,
+        2508
+    );
+    Assertions.assertEquals(5343,_saved_search_order_line.getSaved_Search_Order_ID());
+    Assertions.assertEquals(2508,_saved_search_order_line.getLine_No());
+    Assertions.assertNull(_saved_search_order_line.getCategory_ID());
+    Assertions.assertNull(_saved_search_order_line.getUser_ID());
+    Assertions.assertNull(_saved_search_order_line.getSearch_Phrase());
+    Assertions.assertFalse(_saved_search_order_line.getIs_Active());
+  }
+  @Test
+  public void testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfSaved_Search_Order_IDTooSmall(){
+    int Saved_Search_Order_ID = -1;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setSaved_Search_Order_ID(Saved_Search_Order_ID);});
+  }
+  @Test
+  public void testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfSaved_Search_Order_IDTooBig(){
+    int Saved_Search_Order_ID = 10001;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setSaved_Search_Order_ID(Saved_Search_Order_ID);});
+  }
+  @Test
+  public void testSaved_Search_Order_LineSetsSaved_Search_Order_ID(){
+    int Saved_Search_Order_ID = 9146;
+    _saved_search_order_line.setSaved_Search_Order_ID(Saved_Search_Order_ID);
+    Assertions.assertEquals(Saved_Search_Order_ID, _saved_search_order_line.getSaved_Search_Order_ID());
+  }
+  @Test
+  public void testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfLine_NoTooSmall(){
+    int Line_No = -1;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setLine_No(Line_No);});
+  }
+  @Test
+  public void testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfLine_NoTooBig(){
+    int Line_No = 10001;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setLine_No(Line_No);});
+  }
+  @Test
+  public void testSaved_Search_Order_LineSetsLine_No(){
+    int Line_No = 5512;
+    _saved_search_order_line.setLine_No(Line_No);
+    Assertions.assertEquals(Line_No, _saved_search_order_line.getLine_No());
+  }
+  @Test
+  public void  testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfCategory_IDTooShort(){
+    String Category_ID = "sv";
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setCategory_ID(Category_ID);});
+  }
+  @Test
+  public void  testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfCategory_IDTooLong(){
+    String Category_ID = "mWGariWdWwjpprwRWYbPUCCTvcvNnDFSRuiNNRdaskUyxDRcJTBDmnhpOJgjRJCbIKQenIRywdrOdebloNWKZdmRjEdftPAAQDrNtM";
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setCategory_ID(Category_ID);});
+  }
+  @Test
+  public void testSetCategory_IDSetsCategory_ID(){
+    String Category_ID = "pJobeExhmueXhjfrYNUxxyddRivQSKedQoLgMyfnrxdcSaWAkyKULpbAslOkclDYpfnMNigFAVBRkRvqKPFxcfSNWCWVIpPHTw";
+    _saved_search_order_line.setCategory_ID(Category_ID);
+    Assertions.assertEquals(Category_ID,_saved_search_order_line.getCategory_ID());
+  }
+  @Test
+  public void testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfUser_IDTooSmall(){
+    int User_ID = -1;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setUser_ID(User_ID);});
+  }
+  @Test
+  public void testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfUser_IDTooBig(){
+    int User_ID = 10001;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setUser_ID(User_ID);});
+  }
+  @Test
+  public void testSaved_Search_Order_LineSetsUser_ID(){
+    int User_ID = 2694;
+    _saved_search_order_line.setUser_ID(User_ID);
+    Assertions.assertEquals(User_ID, _saved_search_order_line.getUser_ID());
+  }
+  @Test
+  public void  testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfSearch_PhraseTooShort(){
+    String Search_Phrase = "kh";
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setSearch_Phrase(Search_Phrase);});
+  }
+  @Test
+  public void  testSaved_Search_Order_LineThrowsIllegalArgumentExceptionIfSearch_PhraseTooLong(){
+    String Search_Phrase = "GpMybBwnvkPQmTrCHsASdieMKCdkccaZAbyIWgkLZOZdsmhQoVkIDWLAoXLKTJpQLUjfInrVOASepCSmLhqbvVqLnNDFZCOPYxdTNw";
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_saved_search_order_line.setSearch_Phrase(Search_Phrase);});
+  }
+  @Test
+  public void testSetSearch_PhraseSetsSearch_Phrase(){
+    String Search_Phrase = "PhIKgJyaIrxIZgmmPcvyeRIaPVPWEhZwjapYligMMFLtocfLUjRweVydXmiKEJGBXrpTBavjmGcFVagkrTjflvZHRsCABZZwlS";
+    _saved_search_order_line.setSearch_Phrase(Search_Phrase);
+    Assertions.assertEquals(Search_Phrase,_saved_search_order_line.getSearch_Phrase());
+  }
+  @Test
+  public void testSaved_Search_Order_LineSetsIs_ActiveasFalse(){
+    boolean status = false;
+    _saved_search_order_line.setIs_Active(status);
+    Assertions.assertEquals(status, _saved_search_order_line.getIs_Active());
+  }
+  @Test
+  public void testSaved_Search_Order_LineSetsIs_ActiveasTrue(){
+    boolean status = true;
+    _saved_search_order_line.setIs_Active(status);
+    Assertions.assertEquals(status, _saved_search_order_line.getIs_Active());
+  }
+  @Test
+  public void testCompareToCanCompareForEachDateField() {
+    Saved_Search_Order_Line smaller = new Saved_Search_Order_Line();
+    Saved_Search_Order_Line bigger = new Saved_Search_Order_Line();
+//to compare a smaller and larger Saved_Search_Order_ID
+    smaller.setSaved_Search_Order_ID(10);
+    bigger.setSaved_Search_Order_ID(20);
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Saved_Search_Order_ID as equal.
+    smaller.setSaved_Search_Order_ID(20);
+//to compare a smaller and larger Line_No
+    smaller.setLine_No(10);
+    bigger.setLine_No(20);
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Line_No as equal.
+    smaller.setLine_No(20);
+//to compare a smaller and larger Category_ID
+    smaller.setCategory_ID("aaaa");
+    bigger.setCategory_ID("bbbb");
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Category_ID as equal.
+    smaller.setCategory_ID("bbbb");
+//to compare a smaller and larger User_ID
+    smaller.setUser_ID(10);
+    bigger.setUser_ID(20);
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the User_ID as equal.
+    smaller.setUser_ID(20);
+//to compare a smaller and larger Search_Phrase
+    smaller.setSearch_Phrase("aaaa");
+    bigger.setSearch_Phrase("bbbb");
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Search_Phrase as equal.
+    smaller.setSearch_Phrase("bbbb");
+//to compare a smaller and larger Is_Active
+    smaller.setIs_Active(false);
+    bigger.setIs_Active(true);
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Is_Active as equal.
+    smaller.setIs_Active(true);
+    Assertions.assertTrue(bigger.compareTo(smaller)==0);
+  }
+
+}
+
