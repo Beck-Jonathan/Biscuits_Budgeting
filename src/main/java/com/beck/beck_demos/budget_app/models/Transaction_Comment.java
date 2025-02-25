fd3c86a139ba8bb25feae31e4a8f57966d572606
Beck-Jonathan <jjbeck7@gmail.com>
2025-02-25 10:10:20 -0600

    Transaction Comment object and it's tests.

---
+++ src/main/java/com/beck/beck_demos/budget_app/models/Transaction_Comment.java
@@ -0,0 +1,126 @@
+package com.beck.beck_demos.budget_app.models;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Transaction_Comment implements Comparable<Transaction_Comment> {
+  private Integer User_ID;
+  private Integer Transaction_ID;
+  private Integer Transaction_Comment_ID;
+  private String Content;
+  private Date Post_Date;
+
+  public Transaction_Comment(){}
+
+  public Transaction_Comment(Integer User_ID, Integer Transaction_ID, Integer Transaction_Comment_ID, String Content, Date Post_Date) {
+
+    this.User_ID = User_ID;
+    this.Transaction_ID = Transaction_ID;
+    this.Transaction_Comment_ID = Transaction_Comment_ID;
+    this.Content = Content;
+    this.Post_Date = Post_Date;
+  }
+
+  public Transaction_Comment(Integer User_ID, Integer Transaction_ID, Integer Transaction_Comment_ID) {
+
+    this.User_ID = User_ID;
+    this.Transaction_ID = Transaction_ID;
+    this.Transaction_Comment_ID = Transaction_Comment_ID;
+  }
+  public Integer getUser_ID() {
+    return User_ID;
+  }
+  public void setUser_ID(Integer User_ID) {
+    if (User_ID<0||User_ID>10000){
+      throw new IllegalArgumentException("User_ID Can Not Be Negative");
+    }
+    this.User_ID = User_ID;
+  }
+  public Integer getTransaction_ID() {
+    return Transaction_ID;
+  }
+  public void setTransaction_ID(Integer Transaction_ID) {
+    if (Transaction_ID<0||Transaction_ID>10000){
+      throw new IllegalArgumentException("Transaction_ID Can Not Be Negative");
+    }
+    this.Transaction_ID = Transaction_ID;
+  }
+  public Integer getTransaction_Comment_ID() {
+    return Transaction_Comment_ID;
+  }
+  public void setTransaction_Comment_ID(Integer Transaction_Comment_ID) {
+    if (Transaction_Comment_ID<0||Transaction_Comment_ID>10000){
+      throw new IllegalArgumentException("Transaction_Comment_ID Can Not Be Negative");
+    }
+    this.Transaction_Comment_ID = Transaction_Comment_ID;
+  }
+  public String getContent() {
+    return Content;
+  }
+  public void setContent(String Content) {
+    Content = Content.replaceAll("[^A-Za-z0-9 - ]","");
+    if(Content.length()<4){
+      throw new IllegalArgumentException("Content is too short.");
+    }
+    if(Content.length()>1000){
+      throw new IllegalArgumentException("Content is too long.");
+    }
+    this.Content = Content;
+  }
+  public Date getPost_Date() {
+    return Post_Date;
+  }
+  public void setPost_Date(Date Post_Date)throws ParseException {
+    String minDate = "01/01/1991";
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date _minDate = df.parse(minDate);
+    String maxDate = "12/31/2100";
+    Date _maxDate = df.parse(maxDate);
+    if (Post_Date.compareTo(_minDate)<0){
+      throw new IllegalArgumentException("Post_Date Can Not Be Before 1991");
+    }
+    if (Post_Date.compareTo(_maxDate)>0){
+      throw new IllegalArgumentException("Post_Date Can Not Be after 2100");
+    }
+    this.Post_Date = Post_Date;
+  }
+  @Override
+  public int compareTo(@NotNull Transaction_Comment o) {
+    if (this.User_ID.compareTo(o.User_ID)<0){
+      return -1;
+    }
+    else if(this.User_ID.compareTo(o.User_ID) > 0){
+      return 1;
+    }
+    if (this.Transaction_ID.compareTo(o.Transaction_ID)<0){
+      return -1;
+    }
+    else if(this.Transaction_ID.compareTo(o.Transaction_ID) > 0){
+      return 1;
+    }
+    if (this.Transaction_Comment_ID.compareTo(o.Transaction_Comment_ID)<0){
+      return -1;
+    }
+    else if(this.Transaction_Comment_ID.compareTo(o.Transaction_Comment_ID) > 0){
+      return 1;
+    }
+    if (this.Content.compareTo(o.Content)<0){
+      return -1;
+    }
+    else if(this.Content.compareTo(o.Content) > 0){
+      return 1;
+    }
+    if (this.Post_Date.compareTo(o.Post_Date)<0){
+      return -1;
+    }
+    else if(this.Post_Date.compareTo(o.Post_Date) > 0){
+      return 1;
+    }
+    return 0;
+  }
+
+}
---
+++ src/test/java/com/beck/beck_demos/budget_app/models/Transaction_CommentTest.java
@@ -0,0 +1,195 @@
+package com.beck.beck_demos.budget_app.models;
+
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Transaction_CommentTest {
+
+  private Transaction_Comment _transaction_comment;
+  @BeforeEach
+  public void setup(){
+    _transaction_comment = new Transaction_Comment();
+  }
+  @AfterEach
+  public void teardown(){
+    _transaction_comment = null;
+  }
+  @Test
+  public void testTransaction_CommentDefaultConstructorSetsNoVariables(){
+    Transaction_Comment _transaction_comment= new Transaction_Comment();
+    Assertions.assertNull(_transaction_comment.getUser_ID());
+    Assertions.assertNull(_transaction_comment.getTransaction_ID());
+    Assertions.assertNull(_transaction_comment.getTransaction_Comment_ID());
+    Assertions.assertNull(_transaction_comment.getContent());
+    Assertions.assertNull(_transaction_comment.getPost_Date());
+  }
+  @Test
+  public void testTransaction_CommentParameterizedConstructorSetsAllVariables(){
+    Transaction_Comment _transaction_comment= new Transaction_Comment(
+        4543,
+        5557,
+        1601,
+        "SrRmbRlEMspZgtAPFxXwmgRgRDwOvaxCbNpLvKANSYFQdlZPDUUAsPeZBPPwxrqIfJDJmWiZNMIBZKnfcaNUaaJkxVMBYFQLtaDeeDYaEROtddRbgdekElyGcxFZcCbxiEDbgDyxHnqnhiMwXtsDVpcywqqMWBebEYdBxNAlAynHQokVFFhpZhMygMIljlMLcPcuZfGCVBhHGGRSHlejWCQhndOOBtEMmJfCCjWRLyYFgeYYEZxLrIwgnHyacufhvfQFBOYLRPiwcpcpeOvaREVNbKLwVZxhlIxYiGATphwLQWjxfiGeILjmfNyavlPxtDZqMoawgWXjWtoMWqjrDdwOeDrBmsuhQNvslHshnSDOTJTuGqZbawKmiKGfsTdipSVIIvanqYTAIYZnYQKfvAhScsIgQAuLinQUfAgXhkZlgrZDYDsSroSheqaYYnZdwEdKxRlNrDGnqvIpcpTRBHRBgGHiVVYOETAFMwPaIDjvecIUfQsvxKZMLvymYNEdbEjrWoEByTipIUHUoeylSbVdNyhaHgqCwLIDmeHJZyYjeUrvGfqdolIsIpVZVSfrvMDsBAqbrbvtcdpdAAyYIZyacdoljDgvDmXesNdqmMsaxLoVRNjMPWLFrotypoMKklVIfuaYydEcLthUYpDtXVwMfgUEfaIjxUDcvUbnvnfSbswGaAlRlUoIxamYTeqpEYDOOxdsRsDmWwFAIxfbinmrcMZADKywCGCGlTIOYKKhXSUxOHQfLeBdoHtGkPuyWSZNQZYCuSfqeynylQGofVUSZtchtvtentyHlMxcRJPOHvQJMPxvatJGxIRdkujTAElTtMMGFLdZKgfuGqSExeNpQgXTmvMWKgVRjFnDurZEOoNIkgVtffvElaYaWCyAQYoHNPtilRXNTFjmiomipVKACuHoZDxVXBXKtTQjgqsmuSKMacZvMNXMpKbmPGTHlxRTcnqtQUedILPqWlxEPJDwDJuZakAwBpdmHb",
+        new Date()
+
+    );
+    Assertions.assertEquals(4543,_transaction_comment.getUser_ID());
+    Assertions.assertEquals(5557,_transaction_comment.getTransaction_ID());
+    Assertions.assertEquals(1601,_transaction_comment.getTransaction_Comment_ID());
+    Assertions.assertEquals("SrRmbRlEMspZgtAPFxXwmgRgRDwOvaxCbNpLvKANSYFQdlZPDUUAsPeZBPPwxrqIfJDJmWiZNMIBZKnfcaNUaaJkxVMBYFQLtaDeeDYaEROtddRbgdekElyGcxFZcCbxiEDbgDyxHnqnhiMwXtsDVpcywqqMWBebEYdBxNAlAynHQokVFFhpZhMygMIljlMLcPcuZfGCVBhHGGRSHlejWCQhndOOBtEMmJfCCjWRLyYFgeYYEZxLrIwgnHyacufhvfQFBOYLRPiwcpcpeOvaREVNbKLwVZxhlIxYiGATphwLQWjxfiGeILjmfNyavlPxtDZqMoawgWXjWtoMWqjrDdwOeDrBmsuhQNvslHshnSDOTJTuGqZbawKmiKGfsTdipSVIIvanqYTAIYZnYQKfvAhScsIgQAuLinQUfAgXhkZlgrZDYDsSroSheqaYYnZdwEdKxRlNrDGnqvIpcpTRBHRBgGHiVVYOETAFMwPaIDjvecIUfQsvxKZMLvymYNEdbEjrWoEByTipIUHUoeylSbVdNyhaHgqCwLIDmeHJZyYjeUrvGfqdolIsIpVZVSfrvMDsBAqbrbvtcdpdAAyYIZyacdoljDgvDmXesNdqmMsaxLoVRNjMPWLFrotypoMKklVIfuaYydEcLthUYpDtXVwMfgUEfaIjxUDcvUbnvnfSbswGaAlRlUoIxamYTeqpEYDOOxdsRsDmWwFAIxfbinmrcMZADKywCGCGlTIOYKKhXSUxOHQfLeBdoHtGkPuyWSZNQZYCuSfqeynylQGofVUSZtchtvtentyHlMxcRJPOHvQJMPxvatJGxIRdkujTAElTtMMGFLdZKgfuGqSExeNpQgXTmvMWKgVRjFnDurZEOoNIkgVtffvElaYaWCyAQYoHNPtilRXNTFjmiomipVKACuHoZDxVXBXKtTQjgqsmuSKMacZvMNXMpKbmPGTHlxRTcnqtQUedILPqWlxEPJDwDJuZakAwBpdmHb",_transaction_comment.getContent());
+    Assertions.assertEquals(new Date(),_transaction_comment.getPost_Date());
+  }
+  @Test
+  public void testTransaction_CommentKeyedParameterizedConstructorSetsKeyedVariables(){
+    Transaction_Comment _transaction_comment= new Transaction_Comment(
+        7609,
+        6033,
+        6987
+    );
+    Assertions.assertEquals(7609,_transaction_comment.getUser_ID());
+    Assertions.assertEquals(6033,_transaction_comment.getTransaction_ID());
+    Assertions.assertEquals(6987,_transaction_comment.getTransaction_Comment_ID());
+    Assertions.assertNull(_transaction_comment.getContent());
+    Assertions.assertNull(_transaction_comment.getPost_Date());
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfUser_IDTooSmall(){
+    int User_ID = -1;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setUser_ID(User_ID);});
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfUser_IDTooBig(){
+    int User_ID = 10001;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setUser_ID(User_ID);});
+  }
+  @Test
+  public void testTransaction_CommentSetsUser_ID(){
+    int User_ID = 9791;
+    _transaction_comment.setUser_ID(User_ID);
+    Assertions.assertEquals(User_ID, _transaction_comment.getUser_ID());
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfTransaction_IDTooSmall(){
+    int Transaction_ID = -1;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setTransaction_ID(Transaction_ID);});
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfTransaction_IDTooBig(){
+    int Transaction_ID = 10001;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setTransaction_ID(Transaction_ID);});
+  }
+  @Test
+  public void testTransaction_CommentSetsTransaction_ID(){
+    int Transaction_ID = 1022;
+    _transaction_comment.setTransaction_ID(Transaction_ID);
+    Assertions.assertEquals(Transaction_ID, _transaction_comment.getTransaction_ID());
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfTransaction_Comment_IDTooSmall(){
+    int Transaction_Comment_ID = -1;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setTransaction_Comment_ID(Transaction_Comment_ID);});
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfTransaction_Comment_IDTooBig(){
+    int Transaction_Comment_ID = 10001;
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setTransaction_Comment_ID(Transaction_Comment_ID);});
+  }
+  @Test
+  public void testTransaction_CommentSetsTransaction_Comment_ID(){
+    int Transaction_Comment_ID = 5057;
+    _transaction_comment.setTransaction_Comment_ID(Transaction_Comment_ID);
+    Assertions.assertEquals(Transaction_Comment_ID, _transaction_comment.getTransaction_Comment_ID());
+  }
+  @Test
+  public void  testTransaction_CommentThrowsIllegalArgumentExceptionIfContentTooShort(){
+    String Content = "Yi";
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setContent(Content);});
+  }
+  @Test
+  public void  testTransaction_CommentThrowsIllegalArgumentExceptionIfContentTooLong(){
+    String Content = "bIhiIQejRvVqnIrHOZMdeWBTQQUVKoABqubROryCoffFEcjjeDLFvlfVryQUGGVowNPoybtNaokVNMhdoKvolxpfsAihhQPGVkkCJxGBQHeRffuYAKOZJGbTsGLExFRJEiImpNqoVkUOCHCdrOvHAadZpZeypLJyvleKwWenUMFkEaLiJKIFhwWcqxJboOfFqMEyjVmONkwtuOAZWoTyqEhnUfdlWpvFyiyeKZlipMYhvjJIYYMgNmQdKjIptBouAoLVaPAcUfbvdxWjYkuZUOCTdRWsBJITMDeWMJCuxcnhrrRVKdZfGigyJwlalRQkSEBkXYafNrknFqbswGhKrwLBCVbGKMUfsIAFZQpMNVrOjMjWKVxTxaUeLsedrIKfYroqTvEwRofKuoOBhRdjoFTSclyGQjrIddBWXSVphiodHTnTkhjATkFqlOUfLvFDhcHSidsvVxBMKxKYydQQRaGCrcZfLLYLAyVZnirjCrNlfYXRokvcBbVEqUbQxxXANpbhpDdbPFrDANsWgPXwnEjrIbgmOLbpNDFXyFxxRbSWbriACZlliKgaBFcSdofDIcJsnxksLJKsoENkoDsXwVtfUllfSYOHenApxHWqMmOVCDpyejkuJtFYnqZGyFBDhuQagjXyjhoTnnMTkSuRSsylNjODwovwnZUdMkhiFifAjteUiHWVxguhjgkAXpjUrjbNlpNKGMAUXcFnaYgEROynBUhGoQUCtpHtdjDnPhUOHtcPRrKeMSNxnsVRveSmxtwrkJAxRYmMnxqPcnfekqBhTCCkUuiSYHBejTuBfhrlwINJJNRHtHSyeOWBqrmLUoesjFiKTokWtLkBRMsRjhgIAooFXmgcQiLXjnOOUqfGRKNIuSIlDwiVjKxGsttCyotEoZRRIinVKekQIwYOhUQlEPNxdlESUSepgRleNMnvnnHclFaMInCWKETqWeAqAumegcZHpPkpByxlnGCZPHgRVoJnvMoQZswlBQMHhS";
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setContent(Content);});
+  }
+  @Test
+  public void testSetContentSetsContent(){
+    String Content = "pWfiLkrTLyQERSUPdLhrpUkNHYUoYkbLwTFvsfhixqfJCrdiHqUWCGFDEespcsTRQicayJflmVPRvujVtMnuMVBrNKJpINmokbuFrLjQTjcnTbJZifWDSbikyveHgZaTAdoCBEdsfYDtebfhHQTbqGLNUhcuUmLaefdZlJgJepUtPwbJMoYGxupqPLCiGYAGCrpaUtTMQpSEjAUKVIYgkncxdeujutItiCTelleyKBIaDrhVHiMEWPiQuYvpQSqttdfuFoERcWJrBbSGaEODEEFOejogfIOCBtIlsgLsWCWRiyjTpVbZAjxDrrgiimXDYxoKarbgBimTBpwRRsZBAJJhBoJtSdDNJalfnjrThPQqqlLXtwCOnGJlTXmiEiFqOKmfEjJgsXxgWiuiGLeRwXCWZyKaZybbXloNsqmthgmfAEmVcsMwMUJCSiGiqDyEoQJtrSmCjoOkZMoxleUCbmtJjSLqdnaKmdjZYKeTmbOeOsrMZOaHxICLprLJOhbTNySLKXOyRuFDSxGaHjDTkXXMTlHSYAvfetNPpRIgNvItFsCJAbtxqeHfrtIWmTdgsZhkLEJEorCZWmHIuyOYLKoSnkkaMIEnoXXZsBmEMYVPOKigdAiPpgTERaMNWSSikUXXFPStYVlDwNGIamwyOBCvZLHOcEZZvvNsbunghTUEcbulovbPEsVvvXPrnfMegUiRhhrvqQUZIIrJrDrUeyOOYsQvVPMIGVswKgfAEPCoSjvTagRiaCGoMayAIRBlxLTrKhdtxLclUZBhpWoXtXVFBbOtQwwQAidEfpOZLRbevQvjccSjRFPnXIAyEMRJvDtHAsHwSCeAWyENlATlYxfnWZSOSmdMBTijGKlxCdmBIvlSMppJIUFFhdonSNlmCsDQUIyaVUsmNKCwZhKTdAHJNROVtsPugKwFnRrKEdlpQREhifTfqXICTqcguGBPPkWrRISDOvRXKPGBSwkInBaWhenACTBRqTLSnr";
+    _transaction_comment.setContent(Content);
+    Assertions.assertEquals(Content,_transaction_comment.getContent());
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfPost_DateTooSmall() throws ParseException {
+    String strDate = "03/03/1990";
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date date = df.parse(strDate);
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setPost_Date(date);});
+  }
+  @Test
+  public void testTransaction_CommentThrowsIllegalArgumentExceptionIfPost_DateTooBig() throws ParseException{
+    String strDate = "01/01/2190";
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date date = df.parse(strDate);
+    Assertions.assertThrows(IllegalArgumentException.class, () -> {_transaction_comment.setPost_Date(date);});
+  }
+  @Test
+  public void testTransaction_CommentSetsPost_Date() throws ParseException{
+    String strDate = "25/2/2025";
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date date = df.parse(strDate);
+    _transaction_comment.setPost_Date(date);
+    Assertions.assertEquals(date, _transaction_comment.getPost_Date());
+  }
+  @Test
+  public void testCompareToCanCompareForEachDateField()throws ParseException {
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Transaction_Comment smaller = new Transaction_Comment();
+    Transaction_Comment bigger = new Transaction_Comment();
+//to compare a smaller and larger User_ID
+    smaller.setUser_ID(10);
+    bigger.setUser_ID(20);
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the User_ID as equal.
+    smaller.setUser_ID(20);
+//to compare a smaller and larger Transaction_ID
+    smaller.setTransaction_ID(10);
+    bigger.setTransaction_ID(20);
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Transaction_ID as equal.
+    smaller.setTransaction_ID(20);
+//to compare a smaller and larger Transaction_Comment_ID
+    smaller.setTransaction_Comment_ID(10);
+    bigger.setTransaction_Comment_ID(20);
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Transaction_Comment_ID as equal.
+    smaller.setTransaction_Comment_ID(20);
+//to compare a smaller and larger Content
+    smaller.setContent("aaaa");
+    bigger.setContent("bbbb");
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Content as equal.
+    smaller.setContent("bbbb");
+//to compare a smaller and larger Post_Date
+    smaller.setPost_Date(df.parse("01/01/2023"));
+    bigger.setPost_Date(df.parse("01/01/2024"));
+    Assertions.assertTrue(smaller.compareTo(bigger)<0);
+    Assertions.assertTrue(bigger.compareTo(smaller)>0);
+//to set the Post_Date as equal.
+    smaller.setPost_Date(df.parse("01/01/2024"));
+    Assertions.assertTrue(bigger.compareTo(smaller)==0);
+  }
+
+}
+
